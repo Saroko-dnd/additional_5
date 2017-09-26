f4a87e1a823de3a6893f85bf243a4f1c7546221c
saroko-dnd <isaroko@mail.ru>
2017-09-26 19:04:28 +0300

    Added implementation for check(str, bracketsConfig)

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const stackForBrackets = [];
+  let currentBracketsPair = null;
+
+  for (let index = 0; index < str.length; ++index){
+    currentBracketsPair = bracketsConfig.find((foundBrackets) => {return foundBrackets.includes(str[index]);});
+
+    if (!stackForBrackets.length){
+      if (str[index] !== currentBracketsPair[0]){
+        return false;
+      }else{
+        stackForBrackets.push(str[index]);
+      }
+    }else if (str[index] === currentBracketsPair[1]){
+      if (stackForBrackets[stackForBrackets.length - 1] === currentBracketsPair[0]){
+        stackForBrackets.pop(); 
+      }else if(currentBracketsPair[1] === currentBracketsPair[0]){
+        stackForBrackets.push(str[index]);
+      }else{
+        return false;
+      }
+    }else{
+      stackForBrackets.push(str[index]);
+    }
+  }
+
+  return !stackForBrackets.length;
 }
